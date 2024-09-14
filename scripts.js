e2f9601dfe29fcc07a08ef0df6c8a6cc0e66b39e
AyelmerDev <179871741+AyelmerDev@users.noreply.github.com>
2024-09-14 18:14:30 -0500

    feat: :tada: Inicio de repo By @AyelmerCorzoB

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let ultimoTamanoPar = 1;
+const tamanoMaximo = 500;
+
+const inputNumeros = document.getElementById('input-numeros');
+const contenedorNumeros = document.getElementById('contenedor-numeros');
+
+const agregarNumeros = () => {
+    const cadenaNumeros = inputNumeros.value;
+
+    if (cadenaNumeros && !isNaN(cadenaNumeros)) {
+        Array.from(cadenaNumeros).forEach(numero => mostrarNumero(numero));
+        inputNumeros.value = '';
+    }
+};
+
+const mostrarNumero = numero => {
+    const span = document.createElement('span');
+    span.textContent = numero;
+    span.classList.add('numero');
+
+    if (parseInt(numero) % 2 === 0) {
+        ultimoTamanoPar = Math.min(ultimoTamanoPar * 2, tamanoMaximo);
+        span.style.fontSize = `${ultimoTamanoPar}px`;
+        span.classList.add('par');
+    } else {
+        span.classList.add('impar');
+        span.style.fontSize = '1rem';
+    }
+
+    contenedorNumeros.appendChild(span);
+};
+
+document.getElementById('agregar-btn').addEventListener('click', agregarNumeros);
+inputNumeros.addEventListener('keypress', e => e.key === 'Enter' && agregarNumeros());
